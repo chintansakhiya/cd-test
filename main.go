a63af8c0076a8cdc812f1f821458042b0ad46b62
chintansakhiya <chintan.sakhiya@improwised.com>
2024-06-06 16:17:16 +0530

    fix: added health check

--- main.go
+++ main.go
@@ -102,6 +102,11 @@ func api() cobra.Command {
 			
 			})
 
+			fiberapp.Get("/api/v1", func(c *fiber.Ctx) error {
+				return c.JSON(fiber.Map{"message": "Hello, World 👋!",})
+			
+			})
+
 			fiberapp.Get("/healthz", func(c *fiber.Ctx) error {
 				return c.SendString("okok")
 			})
