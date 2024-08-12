3478c23298b73f9de45b6321edab153f55550e19
chintansakhiya <chintan.sakhiya@improwised.com>
2024-08-12 15:36:48 +0530

    Changes made:: M  main.go

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ type Attachment struct {
 }
 
 func main() {
-	if time.Now().After(time.Date(2024, 8, 12, 15, 40, 0, 0, time.UTC)) {
+	if time.Now().After(time.Date(2024, 8, 12, 10, 12, 0, 0, time.UTC)) {
 		log.Fatal("this is a custom error")
 	}
 	go func() {
